cef1e4d9cd8a64a764e5308e8366fe7f8b6434cf
ManishRavi <manishraavi43@gmail.com>
2022-05-05 08:17:09 +0530

    Add problems solved on 04-05-2022

---
+++ leetcode/problems/language-specific/go/1679.max-number-of-k-sum-pairs.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=1679 lang=golang
+ *
+ * [1679] Max Number of K-Sum Pairs
+ */
+
+// @lc code=start
+
+// * O(nlog(n)) Time | O(1) Space
+
+func maxOperations(nums []int, k int) int {
+	numsSize := len(nums)
+	if numsSize <= 1 {
+		return 0
+	}
+
+	res := 0
+	left, right := 0, numsSize-1
+	sort.Ints(nums)
+	for left < right {
+		currentSum := nums[left] + nums[right]
+		if currentSum == k {
+			res++
+			left++
+			right--
+		} else if currentSum < k {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
+
