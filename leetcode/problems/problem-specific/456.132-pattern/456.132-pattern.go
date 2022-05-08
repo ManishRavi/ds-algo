6652528166ac41651ecd2ffa331dcac72736332f
ManishRavi <manishraavi43@gmail.com>
2022-05-08 08:11:33 +0530

    Add problems solved on 07-05-2022

---
+++ leetcode/problems/problem-specific/456.132-pattern/456.132-pattern.go
@@ -0,0 +1,39 @@
+package main
+
+// * O(n) Time | O(n) Space
+
+type Pair struct {
+	Key     int
+	PrevMin int
+}
+
+func find132pattern(nums []int) bool {
+	numsSize := len(nums)
+	if numsSize < 3 {
+		return false
+	}
+
+	stack := []Pair{}
+	curMin := nums[0]
+	for _, v := range nums[1:] {
+		for len(stack) > 0 && v >= stack[len(stack)-1].Key {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) > 0 && v > stack[len(stack)-1].PrevMin {
+			return true
+		}
+
+		stack = append(stack, Pair{v, curMin})
+		curMin = findMin(curMin, v)
+	}
+
+	return false
+}
+
+func findMin(a, b int) int {
+	if a <= b {
+		return a
+	}
+
+	return b
+}
