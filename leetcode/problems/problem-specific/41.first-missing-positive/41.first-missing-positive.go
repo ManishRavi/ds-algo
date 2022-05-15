30a8936d3c687fa0fd727a4c0d7c4d71cdd37f97
ManishRavi <manishraavi43@gmail.com>
2022-05-15 08:27:39 +0530

    Add problems solved on 14-05-2022

---
+++ leetcode/problems/problem-specific/41.first-missing-positive/41.first-missing-positive.go
@@ -0,0 +1,40 @@
+package main
+
+import "math"
+
+// * Three Pass Solution | O(n) Time | O(1) Space
+
+func firstMissingPositive(nums []int) int {
+	numsSize := len(nums)
+	if numsSize <= 0 {
+		return 1
+	}
+
+	// * Set all negative integers in the array to 0
+	for i, v := range nums {
+		if v < 0 {
+			nums[i] = 0
+		}
+	}
+
+	// * Mark the integers by negating in the array while looping through them
+	for _, v := range nums {
+		curVal := int(math.Abs(float64(v)))
+		if curVal >= 1 && curVal <= numsSize {
+			if nums[curVal-1] > 0 {
+				nums[curVal-1] *= -1
+			} else if nums[curVal-1] == 0 {
+				nums[curVal-1] = -1 * (numsSize + 1)
+			}
+		}
+	}
+
+	// * Find the smallest missing positive integer
+	for i := 1; i < numsSize+1; i++ {
+		if nums[i-1] >= 0 {
+			return i
+		}
+	}
+
+	return numsSize + 1
+}
