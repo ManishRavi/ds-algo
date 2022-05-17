feb2f49da56bac1e0a227ab8793d1e60a2f11a8c
ManishRavi <manishraavi43@gmail.com>
2022-05-17 07:42:35 +0530

    Add problems solved on 16-05-2022

---
+++ leetcode/problems/language-specific/go/1091.shortest-path-in-binary-matrix.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=1091 lang=golang
+ *
+ * [1091] Shortest Path in Binary Matrix
+ */
+
+// @lc code=start
+
+// * Iterative BFS Solution | O(mn) Time | O(mn) Space
+
+func shortestPathBinaryMatrix(grid [][]int) int {
+	if grid[0][0] == 1 {
+		return -1
+	}
+
+	ROWS, COLS := len(grid), len(grid[0])
+	queue := [][]int{}
+	queue = append(queue, []int{0, 0, 1}) // [row, col, length]
+	grid[0][0] = 1
+	dir := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+	for len(queue) > 0 {
+		curSize := len(queue)
+		for curSize > 0 {
+			curCell := queue[0]
+			queue = queue[1:]
+			if curCell[0] == ROWS-1 && curCell[1] == COLS-1 {
+				return curCell[2]
+			}
+
+			for _, v := range dir {
+				row, col := curCell[0]+v[0], curCell[1]+v[1]
+				if row >= 0 && row < ROWS && col >= 0 && col < COLS && grid[row][col] == 0 {
+					queue = append(queue, []int{row, col, curCell[2] + 1})
+					grid[row][col] = 1
+				}
+			}
+
+			curSize--
+		}
+	}
+
+	return -1
+}
+
+// @lc code=end
+
---
+++ leetcode/problems/problem-specific/44.wildcard-matching/44.wildcard-matching.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// * Top-down Memoization Solution | O(mn) Time | O(mn) Space
+
+func isMatch(s string, p string) bool {
+	memcache := map[string]bool{}
+	var isMatchHelperDFS func(i, j int) bool
+	isMatchHelperDFS = func(i, j int) bool {
+		if v, ok := memcache[fmt.Sprintf("%v_%v", i, j)]; ok {
+			return v
+		}
+		if i >= len(s) && j >= len(p) {
+			return true
+		}
+		if j >= len(p) {
+			return false
+		}
+
+		if p[j] == '*' {
+			// * Either don't use "*" --> (i, j+1) or use "*" --> (i+1, j)
+			memcache[fmt.Sprintf("%v_%v", i, j)] = isMatchHelperDFS(i, j+1) ||
+				(i < len(s) && isMatchHelperDFS(i+1, j))
+			return memcache[fmt.Sprintf("%v_%v", i, j)]
+		}
+
+		// * If both chars match
+		if i < len(s) && (s[i] == p[j] || p[j] == '?') {
+			memcache[fmt.Sprintf("%v_%v", i, j)] = isMatchHelperDFS(i+1, j+1)
+			return memcache[fmt.Sprintf("%v_%v", i, j)]
+		}
+
+		memcache[fmt.Sprintf("%v_%v", i, j)] = false
+		return memcache[fmt.Sprintf("%v_%v", i, j)]
+	}
+
+	return isMatchHelperDFS(0, 0)
+}
