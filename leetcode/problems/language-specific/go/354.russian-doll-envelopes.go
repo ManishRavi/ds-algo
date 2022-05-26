6a6355c3af357a3ee116e235fef66cd2ab271725
ManishRavi <manishraavi43@gmail.com>
2022-05-26 13:00:23 +0530

    Add problems solved on 25-05-2022

---
+++ leetcode/problems/language-specific/go/354.russian-doll-envelopes.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=354 lang=golang
+ *
+ * [354] Russian Doll Envelopes
+ */
+
+// @lc code=start
+
+// * Binary Search DP Solution | O(nlogn) Time | O(n) Space
+
+func maxEnvelopes(envelopes [][]int) int {
+	envelopesSize := len(envelopes)
+	// * Sort the envelopes in ascending order based on the width
+	sort.Slice(envelopes, func(i, j int) bool {
+		// * If both the widths are equal then sort in descending order based on the height
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	maxEnvelopes := 0
+	dp := make([]int, envelopesSize)
+	for _, v := range envelopes {
+		// * Find the index to be inserted based on the height
+		index := searchInsert(dp, 0, maxEnvelopes, v[1])
+		dp[index] = v[1]
+		if index == maxEnvelopes {
+			maxEnvelopes++
+		}
+	}
+
+	return maxEnvelopes
+}
+
+func searchInsert(nums []int, left, right, target int) int {
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left
+}
+
+// @lc code=end
+
