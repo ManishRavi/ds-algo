e043ec06f5710ee14277b0332674abfac1a856aa
ManishRavi <manishraavi43@gmail.com>
2022-06-15 09:42:42 +0530

    Add problems solved on 14-06-2022

---
+++ leetcode/problems/language-specific/go/583.delete-operation-for-two-strings.go
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=583 lang=golang
+ *
+ * [583] Delete Operation for Two Strings
+ */
+
+// @lc code=start
+
+// * Bottom-Up Iterative Solution | O(mn) Time | O(mn) Space
+
+func minDistance(word1 string, word2 string) int {
+	word1Size, word2Size := len(word1), len(word2)
+	if word1 == word2 {
+		return 0
+	}
+	if word1Size <= 0 {
+		return word2Size
+	}
+	if word2Size <= 0 {
+		return word1Size
+	}
+
+	dp := make([][]int, word1Size+1)
+	for i := range dp {
+		dp[i] = make([]int, word2Size+1)
+	}
+
+	for i := 0; i <= word1Size; i++ {
+		for j := 0; j <= word2Size; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = i + j
+			} else if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = 1 + findMin(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[word1Size][word2Size]
+}
+
+func findMin(a, b int) int {
+	if a <= b {
+		return a
+	}
+
+	return b
+}
+
+// @lc code=end
+
