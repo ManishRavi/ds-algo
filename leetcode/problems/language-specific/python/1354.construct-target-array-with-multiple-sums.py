a2861c2e78045b356febbf52363597eb067b3c3e
ManishRavi <manishraavi43@gmail.com>
2022-06-25 10:36:33 +0530

    Add problems solved on 24-06-2022

---
+++ leetcode/problems/language-specific/python/1354.construct-target-array-with-multiple-sums.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=1354 lang=python3
+#
+# [1354] Construct Target Array With Multiple Sums
+#
+
+# @lc code=start
+
+# * Priority Queue Solution | O(nlogn) Time | O(n) Space
+
+class Solution:
+    def isPossible(self, target: List[int]) -> bool:
+        max_heap = []
+        for num in target:
+            heappush(max_heap, -num)
+
+        total_sum = sum(target)
+        while max_heap:
+            top_val = -max_heap[0]
+            rem_sum = total_sum-top_val
+            heappop(max_heap)
+            total_sum -= top_val
+            if rem_sum == 1 or top_val == 1:
+                return True
+            if rem_sum > top_val or rem_sum == 0 or top_val % rem_sum == 0:
+                return False
+
+            heappush(max_heap, -(top_val % rem_sum))
+            total_sum += top_val % rem_sum
+
+        return False
+
+# @lc code=end
