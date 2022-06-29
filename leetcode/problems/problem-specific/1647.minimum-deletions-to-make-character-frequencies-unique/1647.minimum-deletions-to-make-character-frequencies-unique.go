b1bd94fe1f186fbaaae59d41d9c0a72a0ec2ad85
ManishRavi <manishraavi43@gmail.com>
2022-06-29 09:50:25 +0530

    Add problems solved on 28-06-2022

---
+++ leetcode/problems/problem-specific/1647.minimum-deletions-to-make-character-frequencies-unique/1647.minimum-deletions-to-make-character-frequencies-unique.go
@@ -0,0 +1,39 @@
+package main
+
+import "sort"
+
+// * Sorting Based Solution | O(n + klogk) Time | O(k) Space
+// * n is the length of the given string and k is the maximum possible number of distinct characters in s
+
+func minDeletions(s string) int {
+	frequencies := make([]int, 26)
+	for _, v := range s {
+		frequencies[v-'a']++
+	}
+
+	sort.Slice(frequencies, func(i, j int) bool {
+		return frequencies[i] > frequencies[j]
+	})
+
+	minCharsToDelete, minFrequencyAllowed := 0, frequencies[0]
+	for _, v := range frequencies {
+		if v > minFrequencyAllowed {
+			minCharsToDelete += v
+			if minFrequencyAllowed > 0 {
+				minCharsToDelete -= minFrequencyAllowed
+			}
+		}
+
+		minFrequencyAllowed = findMin(minFrequencyAllowed-1, v-1)
+	}
+
+	return minCharsToDelete
+}
+
+func findMin(a, b int) int {
+	if a <= b {
+		return a
+	}
+
+	return b
+}
