a2ce6cd0a2d0b7531b4b3018de48e6af02c90622
ManishRavi <manishraavi43@gmail.com>
2022-07-03 09:47:50 +0530

    Add problems solved on 02-07-2022

---
+++ leetcode/problems/language-specific/go/1465.maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=1465 lang=golang
+ *
+ * [1465] Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+ */
+
+// @lc code=start
+
+// * Sorting Based Solution | O(hlogh) + O(wlogw) Time | O(1) Space
+// * h --> Total Height, w --> Total Width
+
+func maxArea(h int, w int, horizontalCuts []int, verticalCuts []int) int {
+	sort.Ints(horizontalCuts)
+	sort.Ints(verticalCuts)
+	maxHorizontalDiff, maxVerticalDiff := findMaxDiff(h, horizontalCuts), findMaxDiff(w, verticalCuts)
+	return (maxHorizontalDiff * maxVerticalDiff) % 1000000007
+}
+
+func findMax(a, b int) int {
+	if a >= b {
+		return a
+	}
+
+	return b
+}
+
+func findMaxDiff(maxSize int, cuts []int) int {
+	cutsSize := len(cuts)
+	maxDiff := cuts[0]
+	for i := 1; i < cutsSize; i++ {
+		maxDiff = findMax(maxDiff, cuts[i]-cuts[i-1])
+	}
+
+	maxDiff = findMax(maxDiff, maxSize-cuts[cutsSize-1])
+	return maxDiff
+}
+
+// @lc code=end
+
