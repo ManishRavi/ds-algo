26f2bff4ef71df3793db5035818987b269719678
ManishRavi <manishraavi43@gmail.com>
2022-07-06 07:56:22 +0530

    Add problems solved on 05-07-2022

---
+++ leetcode/problems/language-specific/go/128.longest-consecutive-sequence.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=128 lang=golang
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+
+// * HashSet and Intelligent Sequence Building Solution | O(n) Time | O(n) Space
+// * n -> Length of the nums array
+
+func longestConsecutive(nums []int) int {
+	numsSize := len(nums)
+	if numsSize <= 1 {
+		return numsSize
+	}
+
+	var exists = struct{}{}
+	mappings, longestConsecutiveSequence := make(map[int]struct{}), 0
+	for _, num := range nums {
+		mappings[num] = exists
+	}
+
+	for _, num := range nums {
+		// * Check if it's the start of the sequence
+		if _, ok := mappings[num-1]; !ok {
+			curElement, curSequenceSize := num, 1
+			for {
+				if _, ok := mappings[curElement+1]; ok {
+					curElement += 1
+					curSequenceSize += 1
+				} else {
+					break
+				}
+			}
+
+			longestConsecutiveSequence = findMax(longestConsecutiveSequence, curSequenceSize)
+		}
+	}
+
+	return longestConsecutiveSequence
+}
+
+func findMax(a, b int) int {
+	if a >= b {
+		return a
+	}
+
+	return b
+}
+
+// @lc code=end
+
