d519d569fbd23ccdf268febf0ce1e095c3d4a2b0
ManishRavi <manishraavi43@gmail.com>
2022-07-21 08:16:01 +0530

    Add problems solved on 20-07-2022

---
+++ leetcode/problems/language-specific/go/792.number-of-matching-subsequences.go
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=792 lang=golang
+ *
+ * [792] Number of Matching Subsequences
+ */
+
+// @lc code=start
+
+// * Iterative HashMap Solution | O(n*(wl)) Time | O(w) Space
+// * n -> Length of s | w -> Number of words | l -> Length of each word
+
+func numMatchingSubseq(s string, words []string) int {
+	sSize := len(s)
+	sMappings := make(map[byte][]string, sSize)
+	res := 0
+	for _, v := range s {
+		vByte := byte(v)
+		sMappings[vByte] = []string{}
+	}
+	for _, word := range words {
+		startChar := word[0]
+		if _, ok := sMappings[startChar]; ok {
+			sMappings[startChar] = append(sMappings[startChar], word)
+		}
+	}
+
+	for _, v := range s {
+		curCharWords := sMappings[byte(v)]
+		curCharWordsSize := len(curCharWords)
+		for i := 0; i < curCharWordsSize; i++ {
+			curWord := curCharWords[i]
+			sMappings[byte(v)] = sMappings[byte(v)][1:]
+			if len(curWord[1:]) == 0 {
+				res++
+			} else if _, ok := sMappings[curWord[1]]; ok {
+				sMappings[curWord[1]] = append(sMappings[curWord[1]], curWord[1:])
+			}
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
+
