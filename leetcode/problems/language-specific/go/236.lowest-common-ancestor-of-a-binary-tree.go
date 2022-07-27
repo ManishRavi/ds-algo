b8ca4c805501bad25cf7170628bff506e464967a
ManishRavi <manishraavi43@gmail.com>
2022-07-27 10:52:11 +0530

    Add problems solved on 26-07-2022

---
+++ leetcode/problems/language-specific/go/236.lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=236 lang=golang
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+
+// * Recursive Solution | O(n) Time | O(n) Space
+// * n -> Number of nodes in a tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	leftNode := lowestCommonAncestor(root.Left, p, q)
+	rightNode := lowestCommonAncestor(root.Right, p, q)
+	if leftNode != nil && rightNode != nil {
+		return root
+	} else if leftNode != nil && rightNode == nil {
+		return leftNode
+	}
+
+	return rightNode
+}
+
+// @lc code=end
+
