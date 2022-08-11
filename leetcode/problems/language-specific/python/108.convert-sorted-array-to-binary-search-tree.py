a20007ddc3238ed32f3743c19d0f6575194188be
ManishRavi <manishraavi43@gmail.com>
2022-08-11 09:13:26 +0530

    Add problems solved on 10-08-2022

---
+++ leetcode/problems/language-specific/python/108.convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=108 lang=python3
+#
+# [108] Convert Sorted Array to Binary Search Tree
+#
+
+# @lc code=start
+
+# * Recursive DFS Solution | O(n) Time | O(h) Space
+# * n -> Length of nums array | h -> Height of a tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+
+        mid = len(nums) >> 1
+        return TreeNode(
+            nums[mid],
+            self.sortedArrayToBST(nums[:mid]),
+            self.sortedArrayToBST(nums[mid+1:])
+        )
+
+# @lc code=end
