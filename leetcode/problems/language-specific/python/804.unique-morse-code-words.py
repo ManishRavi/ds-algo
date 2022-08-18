36673551e4f6f84ba44a7603f1859ae52312655f
ManishRavi <manishraavi43@gmail.com>
2022-08-18 09:18:48 +0530

    Add problems solved on 17-08-2022

---
+++ leetcode/problems/language-specific/python/804.unique-morse-code-words.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=804 lang=python3
+#
+# [804] Unique Morse Code Words
+#
+
+# @lc code=start
+
+# * Hash Table Solution | O(mn) Time | O(mn) Space
+# * m -> The length of words array | n -> The length of each string
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        MORSE_CODES_LOWER = [".-", "-...", "-.-.", "-..", ".", "..-.",
+                             "--.", "....", "..", ".---", "-.-", ".-..",
+                             "--", "-.", "---", ".--.", "--.-", ".-.",
+                             "...", "-", "..-", "...-", ".--", "-..-",
+                             "-.--", "--.."]
+        words_morse_code_set = set()
+        for word in words:
+            transformed_word = ''.join(
+                [MORSE_CODES_LOWER[ord(c) - ord('a')] for c in word]
+            )
+            words_morse_code_set.add(transformed_word)
+
+        return len(words_morse_code_set)
+
+# @lc code=end
---
+++ leetcode/problems/problem-specific/424.longest-repeating-character-replacement/424.longest-repeating-character-replacement.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
+
+# * Two Pointers and Sliding Window Solution | O(n) Time | O(n) Space
+# * n -> The length of s string
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        s_counter = collections.defaultdict(int)
+        max_count = 0
+        longest_substring_length = 0
+        left, right = 0, 0
+        while right < len(s):
+            s_counter[s[right]] += 1
+            max_count = max(max_count, s_counter[s[right]])
+            window_length = right - left + 1
+            # * Difference of window length and max count
+            # * should be less than or equal to k
+            # * (window_length - max_count <= k)
+            if window_length - max_count <= k:
+                longest_substring_length = max(
+                    longest_substring_length, window_length)
+                right += 1
+
+            else:
+                s_counter[s[right]] -= 1
+                s_counter[s[left]] -= 1
+                left += 1
+
+        return longest_substring_length
+
+# @lc code=end
