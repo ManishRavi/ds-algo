c2717c2c8c8e12aab8a2a644464c2fb451c1c4dc
ManishRavi <manishraavi43@gmail.com>
2025-11-15 09:55:25 +0530

    Add problems solved on 14-11-2025

---
+++ leetcode/problems/language-specific/python/1498.number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=1498 lang=python3
+#
+# [1498] Number of Subsequences That Satisfy the Given Sum Condition
+#
+
+# @lc code=start
+
+# * Two Pointers and 1-DP Solution | O(nlogn) Time | O(n) Space
+# * n -> The length of nums array
+
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        res, MOD = 0, 10**9 + 7
+        dp = [1]
+        for _ in range(1, len(nums) + 1):
+            dp.append(dp[-1] << 1 % MOD)
+        
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] + nums[right] <= target:
+                res = (res + dp[right - left]) % MOD
+                left += 1
+            else:
+                right -= 1
+
+        return res
+
+
+# @lc code=end
