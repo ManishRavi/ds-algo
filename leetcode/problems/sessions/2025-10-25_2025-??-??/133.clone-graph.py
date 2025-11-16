589b0a46493e2d46db5687ab660f44b7f10f3a22
ManishRavi <manishraavi43@gmail.com>
2025-11-16 12:18:59 +0530

    Add problems solved on 15-11-2025

---
+++ leetcode/problems/sessions/2025-10-25_2025-??-??/133.clone-graph.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+
+# * Iterative BFS and Hash Table Solution | O(v+e) Time | O(v) Space
+# * v -> The number of vertices in the graph | e -> The number of edges in the graph
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return node
+
+        original_clone_map = defaultdict(Node)
+        queue = deque([node])
+        visited_set = set([node])
+        while queue:
+            cur_node = queue.popleft()
+            if cur_node not in original_clone_map:
+                original_clone_map[cur_node] = Node(cur_node.val)
+
+            for neighbor in cur_node.neighbors:
+                if neighbor not in original_clone_map:
+                    original_clone_map[neighbor] = Node(neighbor.val)
+
+                original_clone_map[cur_node].neighbors.append(
+                    original_clone_map[neighbor]
+                )
+                if neighbor not in visited_set:
+                    queue.append(neighbor)
+                    visited_set.add(neighbor)
+
+        return original_clone_map[node]
+
+
+# @lc code=end
---
+++ leetcode/problems/sessions/2025-10-25_2025-??-??/695.max-area-of-island.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=695 lang=python3
+#
+# [695] Max Area of Island
+#
+
+# @lc code=start
+
+# * Recursive DFS Solution | O(mn) Time | O(1) Space
+# * m -> The number of rows in the grid matrix | n -> The number of columns in the grid matrix
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        max_area = 0
+
+        def helper(row, col):
+            if (
+                not 0 <= row <= ROWS - 1
+                or not 0 <= col <= COLS - 1
+                or grid[row][col] == 0
+            ):
+                return 0
+
+            grid[row][col] = 0
+            cur_area = 1
+            for d_row, d_col in DIRECTIONS:
+                cur_area += helper(row + d_row, col + d_col)
+
+            return cur_area
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1:
+                    max_area = max(max_area, helper(row, col))
+
+        return max_area
+
+
+# @lc code=end
