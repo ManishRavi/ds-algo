691a187ab11d19fbd06d04bf12bf1f598cd6e621
ManishRavi <manishraavi43@gmail.com>
2025-11-25 09:56:45 +0530

    Add problems solved on 24-11-2025

---
+++ leetcode/problems/sessions/2025-10-25_2025-??-??/300.longest-increasing-subsequence.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=300 lang=python3
+#
+# [300] Longest Increasing Subsequence
+#
+
+# @lc code=start
+
+# * Binary Search Solution | O(nlogn) Time | O(n) Space
+# * n -> The length of nums array
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = []
+        for num in nums:
+            if not res or num > res[-1]:
+                res.append(num)
+            else:
+                insert_idx = bisect.bisect_left(res, num)
+                res[insert_idx] = num
+
+        return len(res)
+
+
+# @lc code=end
---
+++ leetcode/problems/sessions/2025-10-25_2025-??-??/416.partition-equal-subset-sum.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=416 lang=python3
+#
+# [416] Partition Equal Subset Sum
+#
+
+# @lc code=start
+
+# * Iterative Bottom-Up Solution | O(n*(sum(nums))) Time | O(sum(nums)) Space
+# * n -> The length of nums array
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        nums_total_sum = sum(nums)
+        nums_half_sum = nums_total_sum // 2
+        if nums_total_sum % 2:
+            return False
+
+        nums_set = set([0])
+        for num in nums:
+            cur_nums_set = nums_set.copy()
+            for v in nums_set:
+                if v + num == nums_half_sum:
+                    return True
+
+                cur_nums_set.add(v + num)
+            nums_set = cur_nums_set
+
+        return False
+
+
+# @lc code=end
